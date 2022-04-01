e26cd56358a60ce2250c8b4f031076ea3680807d
Joseph Akayesi <josephakayesi@gmail.com>
2022-04-01 20:45:19 +0000

    [cleanup]: cleans and modularizes code
    adds ipfs storage class to abstract away ipfs implementation and modularises the code

---
+++ nft/IPFSStorage.js
@@ -0,0 +1,45 @@
+const fs = require('fs')
+const { NFTStorage, Blob } = require('nft.storage')
+const token =
+	process.env.NFT_STORAGE_API_TOKEN ||
+	'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJkaWQ6ZXRocjoweDk3ODMwMGIzNUMzOEM3NzMxYWNDNjk4NDFiODU4NDNiRmIxRjExN0QiLCJpc3MiOiJuZnQtc3RvcmFnZSIsImlhdCI6MTY0ODY2MjM1MDcwNiwibmFtZSI6Im9ydGVsaXVzX3Nib21fbGVkZ2VyX2RlbW8ifQ.egQzHqDB83UoK7ynE4fn4dhgIKWLhPP1B9E6qey4KHM' // your API key from https://nft.storage/manage
+
+class IPFSStorage {
+	constructor(token, endpoint = 'https://api.nft.storage') {
+		this.token = token
+		this.endpoint = endpoint
+        this.storage = new NFTStorage({ endpoint, token })
+	}
+
+	async upload(filePath) {
+		try {
+			const data = await fs.promises.readFile(filePath)
+			const cid = await this.storage.storeBlob(new Blob([data]))
+
+			const metadata = {
+				name: 'example json',
+				description: 'metadata for example json',
+				ipfsUrl: `https://${cid}.ipfs.nftstorage.link/`
+			}
+
+			const status = await this.storage.status(cid)
+
+			return { cid, status, metadata }
+		} catch (error) {
+            console.log(error)
+			throw new Error('unable to upload file')
+		}
+	}
+
+	async retrieve(cid) {
+		try {
+			const status = await this.storage.status(cid)
+
+			if (status.cid) return { ipfsUrl: `https://${cid}.ipfs.nftstorage.link/` }
+		} catch (error) {
+			throw new Error('file not found')
+		}
+	}
+}
+
+module.exports = new IPFSStorage(token)
--- nft/index.js
+++ nft/index.js
@@ -1,32 +1,18 @@
-const fs = require('fs')
-const { NFTStorage, Blob } = require('nft.storage')
 const dotenv = require('dotenv')
+const ipfsStorage = require('./IPFSStorage')
 
 dotenv.config()
 
-const endpoint = 'https://api.nft.storage' // the default
-const token =
-	process.env.NFT_STORAGE_API_TOKEN ||
-	'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJkaWQ6ZXRocjoweDk3ODMwMGIzNUMzOEM3NzMxYWNDNjk4NDFiODU4NDNiRmIxRjExN0QiLCJpc3MiOiJuZnQtc3RvcmFnZSIsImlhdCI6MTY0ODY2MjM1MDcwNiwibmFtZSI6Im9ydGVsaXVzX3Nib21fbGVkZ2VyX2RlbW8ifQ.egQzHqDB83UoK7ynE4fn4dhgIKWLhPP1B9E6qey4KHM' // your API key from https://nft.storage/manage
-
 async function main() {
-	const storage = new NFTStorage({ endpoint, token })
+	try {
+		const { cid } = await ipfsStorage.upload('./example.json')
 
-	const data = await fs.promises.readFile('example.json')
-	const cid = await storage.storeBlob(new Blob([data]))
-	console.log({ cid })
+		const { ipfsUrl } = await ipfsStorage.retrieve(cid)
 
-	const metadata = {
-		name: 'example json',
-		description: 'metadata for example json',
-		ipfs_url: `https://${cid}.ipfs.nftstorage.link/`
+		console.log(`ipsUrl: ${ipfsUrl}`)
+	} catch (error) {
+		console.log(`error: ${error.message}`)
 	}
-
-	const status = await storage.status(cid)
-	console.log(status)
-
-	// metadata is stored on blockchain
-	console.log(metadata)
 }
 
 main()
