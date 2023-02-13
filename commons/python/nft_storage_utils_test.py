eb9bde3d18436c9bb50febae69b056258a953fdb
Utkarsh Kumar Sharma <utkarshkviim@gmail.com>
2023-02-14 00:41:28 +0530

    enchance get_minimize_data method
    Signed-off-by: Utkarsh Kumar Sharma <utkarshkviim@gmail.com>

--- commons/python/nft_storage_utils_test.py
+++ commons/python/nft_storage_utils_test.py
@@ -1,12 +1,14 @@
 from nft_storage_utils import *
 
 response = save('../../test/resource/sample_sbom_1.json', 'file')
+print(response)
 assert response['ok'] == True
-assert response['value']['cid'] == 'bafkreibcqaowdyb47fqzlsk5lsj74uhnu6gfqecpswv2m3kmw2cbkkq2be'
+assert response['value']['cid'] == 'bafkreidxuo3gms7zxtdl5dnrhjtbzql4jm622kywkdaddocba5y5ekmuz4'
 
 response = save('../../test/resource/sample_sbom_2.json', 'file')
+print(response)
 assert response['ok'] == True
-assert response['value']['cid'] == 'bafkreibcqaowdyb47fqzlsk5lsj74uhnu6gfqecpswv2m3kmw2cbkkq2be'
+assert response['value']['cid'] == 'bafkreidxuo3gms7zxtdl5dnrhjtbzql4jm622kywkdaddocba5y5ekmuz4'
 
 josn_dict = {
     "licenses": [
--- commons/python/utils.py
+++ commons/python/utils.py
@@ -1,4 +1,33 @@
 import json
 
 def get_minimize_data(jsonData):
-  return json.dumps(jsonData, separators=(',', ':'))
+  sorted_by_key = get_sorted(jsonData)
+  print(sorted_by_key)
+  return json.dumps(sorted_by_key, separators=(',', ':'))
+
+
+def sort_dict_by_key(dictionary):
+  sorted_keys = sorted(dictionary.keys())
+  return {key:dictionary[key] for key in sorted_keys}
+
+def get_sorted(dictionaryOrList):
+  for key in dictionaryOrList:
+    if(isinstance(dictionaryOrList[key], list) and dictionaryOrList[key].__len__() != 0 and isinstance(dictionaryOrList[key][0], dict)):
+      collector = []
+      for element in dictionaryOrList[key]:
+        collector.append(get_sorted(element))
+    
+      dictionaryOrList[key] = collector
+
+    elif(isinstance(dictionaryOrList[key], list) and dictionaryOrList[key].__len__() != 0 and isinstance(dictionaryOrList[key][0], str)):
+      dictionaryOrList[key].sort()
+
+    elif(isinstance(dictionaryOrList[key], list) and dictionaryOrList[key].__len__() != 0 and isinstance(dictionaryOrList[key][0], int)):
+      dictionaryOrList[key].sort()
+
+    elif(isinstance(dictionaryOrList[key], dict)):
+      dictionaryOrList[key] = get_sorted(dictionaryOrList[key])
+    
+  return sort_dict_by_key(dictionaryOrList)
+
+    
